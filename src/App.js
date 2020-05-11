b848fdf40c02b6c6f0de750b0ff819ed8a71937c
Abhishek_Rai <raigon48@gmail.com>
2020-05-11 16:37:40 +0530

    Added count and timer logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const DEFAULT_TIME = 5;
+  const [text, setText] = useState('');
+  const [timeRemaining, setTimeRemaining] = useState(DEFAULT_TIME);
+  const [gameRunning, setGameRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+  var count = 0;
+
+  useEffect(()=> {
+      if(gameRunning && timeRemaining > 0){
+        setTimeout(()=> {
+          setTimeRemaining((prevTime) => prevTime - 1);
+        }, 1000)
+      } else if(timeRemaining === 0) {
+        endGame();
+      }
+    
+  },[timeRemaining, gameRunning])
+
+  function startGame() {
+    setGameRunning(true);
+    setTimeRemaining(DEFAULT_TIME);
+    setWordCount(0);
+  }
+
+  function endGame() {
+    const count = countWords(text)
+    setWordCount(count);
+    setGameRunning(false);
+    setTimeRemaining(0);
+  }
+
+  function handleChange(event) {
+    const {value} = event.target;
+    setText(value)
+  }
+
+  function countWords(text){
+    const wordArray = text.split(' ');
+    return wordArray.filter(word => word !== '').length;
+  }
+
+
   return (
     <div>
-      Hello
+      <h1>Speed Typing</h1>
+      <textarea placeholder="Start Typing Here..." onChange={handleChange} disabled={!gameRunning}/>
+      <h4>Time Remaining : {timeRemaining}</h4>
+      <button onClick={startGame}  disabled={gameRunning}>{gameRunning ? '....' : 'Start'}</button>
+      <h1>Word Count : {wordCount}</h1>
     </div>
   );
 }
